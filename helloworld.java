af6fd578b5b75f2a532d2eeb88f7a5e7591c69b7
knvpkgithub <pavankumar.gem@com>
2023-02-13 15:16:23 +0530

    file created

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,8 @@
 //hello welcome to git
+class helloworld
+{
+public static void main(String args[])
+{
+System.out.println("classpathdemo");
+}
+}
---
+++ sales.java
@@ -0,0 +1,5 @@
+public class sales {
+
+    // sales process
+    
+}
